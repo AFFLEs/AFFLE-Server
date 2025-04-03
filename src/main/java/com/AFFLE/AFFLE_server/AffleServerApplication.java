10451edc16c41dbe4e7c810c854f89d0b7603f70
dudtlstm <annpys1024@naver.com>
2025-04-03 13:33:09 +0900

    [Feature] #1 - 모든 파일 추가

---
+++ src/main/java/com/AFFLE/AFFLE_server/AffleServerApplication.java
@@ -0,0 +1,13 @@
+package com.AFFLE.AFFLE_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AFFLE/AFFLE_server/AffleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AFFLE.AFFLE_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
